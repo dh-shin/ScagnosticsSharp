10f6318a7ab5e542b630afc0bb77057e019d1f66
DongHWa Shin <dhshin@hcil.snu.ac.kr>
2017-06-02 00:37:20 +0900

    Downgrade target .NET Framework version to 4.0

--- src/ScagnosticsSharp.Test/Properties/AssemblyInfo.cs
+++ src/ScagnosticsSharp.Test/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScagnosticsSharp")]
+[assembly: AssemblyTitle("ScagnosticsSharp.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScagnosticsSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("ScagnosticsSharp.Test")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
